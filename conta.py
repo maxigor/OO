bdb2ba14c71fc3b4944e3f5df95551bc77c4dfb6
Max <maxigor.ferreira@gmail.com>
2021-06-23 17:08:20 -0300

    add getters and setters

---
+++ conta.py
@@ -0,0 +1,36 @@
+class Conta:
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto... {}".format(self))
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def get_extrato(self):
+        print("Saldo {} ".format(self.saldo))
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saca(self, valor):
+        self.saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    @property
+    def saldo(self, saldo):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
